f42692dc30ad013393c39640dd0a44da5f5b721b
Marcelo Schlickmann <20998080+M4schlickmann@users.noreply.github.com>
2019-04-26 15:26:19 -0300

    Update captura.py

--- captura.py
+++ captura.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-##list of all classifier##
+#list dos classificadores
 
 classificador = cv2.CascadeClassifier('haarcascade-frontalface-default.xml')
 classificadorOlho = cv2.CascadeClassifier('haarcascade-eye.xml')
@@ -12,13 +12,13 @@ id = input('Digite o identificador')
 largura, altura = 220, 220
 print('Capturando as faces...')
 
-##select cam.##
+#seleciona a câmera que vai ser usada para a captura
 while (True):
     conectado, imagem = camera.read()
     imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
     print(np.average(imagemCinza))
     facesDetectadas = classificador.detectMultiScale(imagemCinza, scaleFactor=1.5, minSize=(150, 150))
-##drow a new window##
+#constroi uma nova janela para fazer o desenho nas faces
     for (x, y, l, a) in facesDetectadas:
         cv2.rectangle(imagem, (x, y), (x+l, y+a), (0, 139, 139), 2)
         '''regiao = imagem[y:y + a, x:x + l]
@@ -26,7 +26,7 @@ while (True):
         olhosdetectados = classificadorOlho.detectMultiScale(regiaoCinzaOlho)
         for (ox, oy, ol, oa) in olhosdetectados:
             cv2.rectangle(regiao, (ox, oy), (ox + ol, oy + oa), (0, 255, 0), 2)'''
-#condition to close window## 
+#condição para fechar a tela de desenho de faces
         if cv2.waitKey(1) & 0xFF == ord('q'):
             if np.average(imagemCinza) > 90:
                 imagemFace = cv2.resize(imagemCinza[y:y + a, x:x + l], (largura, altura))
