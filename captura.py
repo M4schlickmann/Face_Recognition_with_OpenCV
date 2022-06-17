12c96f2ed9e1aaa7f1893f4cca79ad2f80ea1c6b
Marcelo Schlickmann <20998080+M4schlickmann@users.noreply.github.com>
2022-06-17 14:01:19 -0300

    Update captura.py
    Tipificação de cada setor

--- captura.py
+++ captura.py
@@ -5,20 +5,26 @@ import numpy as np
 
 classificador = cv2.CascadeClassifier('haarcascade-frontalface-default.xml')
 classificadorOlho = cv2.CascadeClassifier('haarcascade-eye.xml')
-camera = cv2.VideoCapture(0)
+
+#Momento de captura das faces para alimentação do banco
+camera = cv2.VideoCapture(0) 
 amostra = 1
 numeroAmostra = 25
+
+#identificador para separação das faces
 id = input('Digite o identificador')
 largura, altura = 220, 220
 print('Capturando as faces...')
 
-#seleciona a câmera que vai ser usada para a captura
+#seleciona a câmera que vai ser usada para a captura das imagens
+
 while (True):
     conectado, imagem = camera.read()
     imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
     print(np.average(imagemCinza))
     facesDetectadas = classificador.detectMultiScale(imagemCinza, scaleFactor=1.5, minSize=(150, 150))
-#constroi uma nova janela para fazer o desenho nas faces
+    
+#Modela uma nova janela retangular delimitando a área da face
     for (x, y, l, a) in facesDetectadas:
         cv2.rectangle(imagem, (x, y), (x+l, y+a), (0, 139, 139), 2)
         '''regiao = imagem[y:y + a, x:x + l]
@@ -26,7 +32,9 @@ while (True):
         olhosdetectados = classificadorOlho.detectMultiScale(regiaoCinzaOlho)
         for (ox, oy, ol, oa) in olhosdetectados:
             cv2.rectangle(regiao, (ox, oy), (ox + ol, oy + oa), (0, 255, 0), 2)'''
-#condição para fechar a tela de desenho de faces
+        
+#Condição para o encerramento da tela de desenho
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             if np.average(imagemCinza) > 90:
                 imagemFace = cv2.resize(imagemCinza[y:y + a, x:x + l], (largura, altura))
@@ -38,6 +46,7 @@ while (True):
     cv2.waitKey(1)
     if (amostra >= numeroAmostra + 1):
         break
+#Mensagem que aparecerá após cuncluir a captura das faces
 
 print('Faces capturadas com sucesso..')
 camera.release()
